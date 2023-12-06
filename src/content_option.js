625f0ae87b8c3c755c3f815b9a2fd98659e6345d
Younessallami <younes.sallami@esprit.tn>
2023-12-06 23:57:57 +0100

    ala sormek baed ma tgueedli lfaza hethy brojla

--- src/content_option.js
+++ src/content_option.js
@@ -1,47 +1,32 @@
-const logotext = "JOHN";
+const logotext = "YOON";
 const meta = {
-    title: "John Doe",
+    title: "Younes Sallami",
     description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Younes Sallami",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Tech Engineer & Artist",
+        second: "Visual Art Creator",
+        third: " Game Developer & Innovator",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Passionate about crafting immersive gaming experiences, I bring a unique blend of technical expertise and artistic creativity. Specializing in seamlessly integrating cutting-edge technology with captivating visuals, I consistently elevate game aesthetics. As a dedicated Game Developer & Innovator, I thrive on pushing boundaries, ensuring each project is a testament to innovation in the gaming world.",
+    your_img_url: "https://i.ibb.co/nz697GC/Black-and-White-Mountains-and-Leaf-on-Model-Framed-Art-Print.png",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "About My Self",
+    aboutme: "Hello, I'm Younes Sallami, fueled by a lifelong passion for video games and visual art. From childhood, I immersed myself in the intricate world of game development, driven by a fascination with the entire creative process. Beyond playing masterpieces, my journey began with editing shaders and crafting artistic video clips, contributing to various events. After obtaining my bachelor's degree in mechanical engineering, I made a decisive shift, pursuing my primary hobby with a dedicated engineering course in computer science, specializing in gaming development and graphical design at The Private Higher School of Engineering and Technology.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
-];
+
 
 const skills = [{
-        name: "Python",
+        name: "C#",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Blueprint",
         value: 85,
     },
     {
@@ -49,96 +34,61 @@ const skills = [{
         value: 80,
     },
     {
-        name: "React",
+        name: "C + +",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Dart",
+        value: 70,
     },
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Adope",
+        description: "I specialize in creating captivating visual stories with Adobe Premiere, offering video editing services that enhance your content, from cinematic storytelling to polished promotional videos.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Games & Simulations & Virtual reality",
+        description: "Unlocking immersive experiences: Leveraging Unreal Engine and Unity, I craft cutting-edge virtual worlds and interactive simulations, offering services that bring your ideas to life in the realms of gaming, simulations, and virtual reality.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Web & Mobile Application",
+        description: "Specializing in Flutter, Node.js, and Symphony, I offer comprehensive services to develop high-performance applications, ensuring a harmonious blend of efficiency and innovation in your digital ventures.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.ibb.co/JyrYHxp/The-Heist.png",
+        description: "Bank heist is a VR Desktop game that involves around robbing a bank ",
+        link: "https://www.youtube.com/watch?v=8O1NvM041e0",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.ibb.co/k5z7wds/White-and-Blue-UFO-Future-Modern-Movie-Poster.png",
+        description: "Fleshpit is a third person perspective mobile mul-tiplayer.",
+        link: "https://www.youtube.com/watch?v=ha9QjYkZ-2k",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.ibb.co/X8bjG88/IMG-0642.jpg",
+        description: "Esmod project directed by younes sallami",
+        link: "https://www.instagram.com/p/CfcD9KhA-ZW/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==",
     },
+   {
+        img: "https://i.ibb.co/TKyK78X/Bold.png",
+        description: "Behind the scenes and making of with Production company SMURFS production  .",
+        link: "#https://www.instagram.com/reel/Chkf-L1jvaY/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==",
+    },
+   
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+        img: "https://i.ibb.co/w7xSF1X/Sci-Fi-Game-Kit-Unreal-Editor-2023-12-06-21-00-32-00-00-02-19-Still001.gif",
+        description: "AudioReactive Simulation RealTime Work This is an Exemple I will import athor's later .",
+        link: "https://drive.google.com/file/d/1xZ9NX4n6UAAzFMBdvpi0O7k4INcc9nPb/view?usp=sharing",
+    }
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "younes.sallami@esprit.tn",
+    YOUR_FONE: "(216)560-66956",
+    description: "Thank you for entrusting me with your projects . it's been a pleasure working together to bring ideas to life and achieve shared success . ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -147,16 +97,16 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    github: "https://github.com/Younessallami",
+    facebook: "https://www.facebook.com/profile.php?id=100004350934826",
+    linkedin: "https://www.linkedin.com/in/younes-sallami-b07743257/",
     twitter: "https://twitter.com",
 };
 export {
     meta,
     dataabout,
     dataportfolio,
-    worktimeline,
+    
     skills,
     services,
     introdata,
